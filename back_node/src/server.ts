b634cb6b6ad0b9f10a0ef3511fa4b6b01f33a44f
= <=>
2026-08-19 14:25:45 -0500

    back: vercel fixment?

--- back_node/src/server.ts
+++ back_node/src/server.ts
@@ -3,25 +3,28 @@ import { AppRoutes } from './routes';
 import cors from "cors";
 import { MatchCronProcess } from './modules/match/match.cron';
 
+export const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        'http://localhost:4200',
+        'https://polla-node-4yvx-git-master-felipe12357s-projects.vercel.app'
+    ]
+}));
+app.use(AppRoutes.routes);
+
 export class Server {
-    private app = express();
     private PORT = 3000;
-    private routes = AppRoutes.routes;
-    private cors = cors;
 
     static async start(){
 
         MatchCronProcess.updateMatchProcess();
 
-        //use, significa q es un middleware
-        const serverInstance = new Server();
-        serverInstance.app.use(express.json()); //serializa los body de las peticiones a json
-        serverInstance.app.use(express.urlencoded({extended:true})) // serializa los body de las peticiones x-www-form-urlencode
-        serverInstance.app.use(serverInstance.cors({ origin: ['http://localhost:5173', 'http://localhost:4200', 'https://polla-node-4yvx-git-master-felipe12357s-projects.vercel.app'] }))
-        serverInstance.app.use(serverInstance.routes)
-
-        serverInstance.app.listen(serverInstance.PORT, () => {
-            console.log(`Server running at http://localhost:${serverInstance.PORT}` );
+        app.listen(new Server().PORT, () => {
+            console.log(`Server running at http://localhost:${new Server().PORT}` );
         });
     }
 }
