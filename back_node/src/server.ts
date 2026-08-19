55fa74d92168fb62b5cc2e39bd92d86b2c7bdf69
= <=>
2026-08-19 17:22:23 -0500

    test: vercel unnecessary changes

--- back_node/src/server.ts
+++ back_node/src/server.ts
@@ -3,12 +3,43 @@ import { AppRoutes } from './routes';
 import cors from "cors";
 // import { MatchCronProcess } from './modules/match/match.cron';
 
-export const app = express();
+export class Server {
+    private app = express();
+    private PORT = 3000;
+    private routes = AppRoutes.routes;
+    private cors = cors;
+
+    static async start(){
+
+       // MatchCronProcess.updateMatchProcess();
+
+        //use, significa q es un middleware
+        const serverInstance = new Server();
+        serverInstance.app.use(express.json()); //serializa los body de las peticiones a json
+        serverInstance.app.use(express.urlencoded({extended:true})) // serializa los body de las peticiones x-www-form-urlencode
+        serverInstance.app.use(serverInstance.cors({ origin: [
+            'http://localhost:5173',
+            'http://localhost:4200',
+            'polla-node-4yvx.vercel.app',
+            'http://polla-node-4yvx.vercel.app',
+            'https://polla-node-4yvx.vercel.app',
+            'https://polla-node-4yvx-git-master-felipe12357s-projects.vercel.app'
+        ] }))
+        serverInstance.app.use(serverInstance.routes)
+
+        serverInstance.app.listen(serverInstance.PORT, () => {
+            console.log(`Server running at http://localhost:${serverInstance.PORT}` );
+        });
+    }
+}
+// import { MatchCronProcess } from './modules/match/match.cron';
+
+/* export const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-/* app.use(cors({
+app.use(cors({
     origin: [
         'http://localhost:5173',
         'http://localhost:4200',
@@ -17,7 +48,7 @@ app.use(cors());
         'https://polla-node-4yvx.vercel.app',
         'https://polla-node-4yvx-git-master-felipe12357s-projects.vercel.app'
     ]
-})); */
+}));
 app.get('/api/test', (req, res) => {
   res.json({
     ok: true,
@@ -26,7 +57,7 @@ app.get('/api/test', (req, res) => {
 });
 app.use(AppRoutes.routes);
 
-/* export class Server {
+ export class Server {
     private PORT = 3000;
 
     static async start(){
@@ -37,4 +68,4 @@ app.use(AppRoutes.routes);
             console.log(`Server running at http://localhost:${new Server().PORT}` );
         });
     }
-} */
+}  */
